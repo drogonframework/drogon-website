69b9a066b9618938288645c54a5bf026b172d97d
fingerecho <283285356@qq.com>
2020-09-19 19:30:44 +0800

    upate with 'scrolls' template

---
+++ docs/.scrolls_build/dirhtml/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v1.0.0-beta21',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
